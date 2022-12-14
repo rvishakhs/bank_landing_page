1d4762eba55e7c20aff058694aaa5021d8f17c30
rvishakhs <rvishakhs@gmail.com>
2022-12-14 23:53:04 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-primary  w-full h-fit'>
+      <h1 className='font-semibold text-2xl text-cyan-50'>Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
