7d9b77e11b47771859cb79b52949a81f68920772
rvishakhs <rvishakhs@gmail.com>
2022-12-15 23:46:13 +0000

    something wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from './style'
 
 function App() {
   return (
     <div className='bg-primary  w-full h-fit'>
-      <h1 className='font-semibold text-2xl text-cyan-50'>Hello world!</h1>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            hero
+          </div>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            Card Section 
+            Billig Sefction 
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
